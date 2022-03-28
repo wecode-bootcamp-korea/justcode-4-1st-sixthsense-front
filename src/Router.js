ed196797f6b5d51f9d1e8914487ff3bd2ccbe445
김수빈 <ksb334ksb@gmail.com>
2022-03-28 18:00:39 +0900

    Fix: 라우팅 경로 추가

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Nav from './components/nav/Nav';
+import Footer from './components/footer/Footer';
+
+import Main from './pages/main/Main';
+import List from './pages/list/List';
+import Detail from './pages/detail/Detail';
+import LogIn from './pages/login/LogIn';
+import SignUp from './pages/signup/SignUp';
+import MyPage from './pages/mypage/Mypage';
+
 function Router() {
   return (
     <BrowserRouter>
-      {/* <Nav /> */}
-      <Routes>{/* <Route path="/" element={<Login />} /> */}</Routes>
-      {/* <Footer /> */}
+      <Nav />
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/like/Like.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Like() {
+  return <div />;
+}
+
+export default Like;
