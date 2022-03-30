f88ed03097c903f6d364ddd8eb6eb1f6a6822751
김수빈 <ksb334ksb@gmail.com>
2022-03-30 17:27:02 +0900

    add : nav modal layout

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,29 +1,60 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Modal from './Modal';
 import css from './Nav.module.css';
 
 function Nav() {
+  const [modalActive, setmodalActive] = useState(0);
+  const modalRef = useRef();
+
+  const openModal = () => setmodalActive(1);
+  const closeModal = () => setmodalActive(0);
+
+  useEffect(() => {
+    if (modalActive) {
+      modalRef.current.style.display = 'flex';
+    } else {
+      modalRef.current.style.display = 'none';
+    }
+  }, [modalActive]);
+
   return (
-    <div className={css.navWrapper}>
-      <nav className={css.nav}>
-        <div className={css.logo}>logo</div>
-        <div className={css.modals}>
-          <div className={css.modalWrapper}>
-            <p>icon1 어디로떠날까요?</p>
+    <>
+      <div className={css.navWrapper}>
+        <nav className={css.nav}>
+          <div className={css.logo}>logo</div>
+          <div className={css.centerMenu}>
+            <p id="modal1" className={css.modalButton} onClick={openModal}>
+              icon1 어디로떠날까요?
+            </p>
+            <p id="modal2" className={css.modalButton} onClick={openModal}>
+              icon2 언제떠날까요?
+            </p>
           </div>
-          <div className={css.modalWrapper}>
-            <p>icon2 언제떠날까요?</p>
+          <div className={css.rightMenu}>
+            <Link to="/" className={css.menuList}>
+              FIND STAY
+            </Link>
+            <Link to="/" className={css.menuList}>
+              PROMOTION
+            </Link>
+            <Link to="/" className={css.menuList}>
+              JOURNAL
+            </Link>
+            <Link to="/" className={css.menuList}>
+              PRE-ORDER
+            </Link>
+            <Link to="/" className={css.menuList}>
+              LOGIN
+            </Link>
+            <Link to="/" className={css.menuList}>
+              icon
+            </Link>
           </div>
-        </div>
-        <ul className={css.menu}>
-          <li className={css.menuList}>FIND STAY</li>
-          <li className={css.menuList}>PROMOTION</li>
-          <li className={css.menuList}>JOURNAL</li>
-          <li className={css.menuList}>PRE-ORDER</li>
-          <li className={css.menuList}>LOGIN</li>
-          <li className={css.menuList}>icon</li>
-        </ul>
-      </nav>
-    </div>
+        </nav>
+      </div>
+      <Modal closeModal={closeModal} modalRef={modalRef} />
+    </>
   );
 }
 
