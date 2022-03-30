ee1c805f153cc2c2c9f42e9bbcd24fb6176934e9
김수빈 <ksb334ksb@gmail.com>
2022-03-30 16:09:07 +0900

    add : mainpage layout & nav layout

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import css from './Nav.module.css';
 
 function Nav() {
-  return <div />;
+  return (
+    <div className={css.navWrapper}>
+      <nav className={css.nav}>
+        <div className={css.logo}>logo</div>
+        <div className={css.modals}>
+          <div className={css.modalWrapper}>
+            <p>icon1 어디로떠날까요?</p>
+          </div>
+          <div className={css.modalWrapper}>
+            <p>icon2 언제떠날까요?</p>
+          </div>
+        </div>
+        <ul className={css.menu}>
+          <li className={css.menuList}>FIND STAY</li>
+          <li className={css.menuList}>PROMOTION</li>
+          <li className={css.menuList}>JOURNAL</li>
+          <li className={css.menuList}>PRE-ORDER</li>
+          <li className={css.menuList}>LOGIN</li>
+          <li className={css.menuList}>icon</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import css from './Main.module.css';
 
 function Main() {
-  return <div />;
+  return (
+    <>
+      <div className={`${css.sliderBig} ${css.first}`}>slider big 1</div>
+      <div className={css.sliderSmall}>slider small 1</div>
+      <div className={css.banner}>banner</div>
+      <div className={css.sliderMedium}>slider medium 1</div>
+      <div className={css.sliderSmall}>slider small 2</div>
+      <div className={css.sliderBig}>slider big 2</div>
+      <div className={css.travelList}>travel list</div>
+      <div className={css.hashtagList}>hashtag list</div>
+      <div className={css.sliderSmall}>slider small 3</div>
+      <div className={css.sliderSmall}>slider small 4</div>
+      <div className={css.contact}>contact</div>
+    </>
+  );
 }
 
 export default Main;
