92f889c4483a31ad79144092d8634ebcc04a4f39
김수빈 <ksb334ksb@gmail.com>
2022-04-01 01:07:39 +0900

    Modify: react-icons 추가(버전 근홍님것과 동일) , 어디로modal layout 제작

--- src/components/nav/modal/WhereModal.js
+++ src/components/nav/modal/WhereModal.js
@@ -15,6 +15,46 @@ function WhereModal({ modalRef, closeModal }) {
             </div>
           </div>
           <input />
+          <div className={style.location}>
+            <div className={style.locationWrapper}>
+              <p className={style.domestic}>국내</p>
+              <ul>
+                <li>국내전체</li>
+                <li>제주</li>
+                <li>서울</li>
+                <li>강원</li>
+                <li>부산</li>
+                <li>경기</li>
+                <li>충청</li>
+                <li>경상</li>
+                <li>전라</li>
+                <li>인천</li>
+                <li>광주</li>
+                <li>대전</li>
+                <li>대구</li>
+                <li>울산</li>
+              </ul>
+            </div>
+            <div className={style.locationWrapper}>
+              <p className={style.domestic}>국내</p>
+              <ul>
+                <li>국내전체</li>
+                <li>제주</li>
+                <li>서울</li>
+                <li>강원</li>
+                <li>부산</li>
+                <li>경기</li>
+                <li>충청</li>
+                <li>경상</li>
+                <li>전라</li>
+                <li>인천</li>
+                <li>광주</li>
+                <li>대전</li>
+                <li>대구</li>
+                <li>울산</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
