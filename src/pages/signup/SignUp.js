8935ef22a5d850e18a0bf89ab4fbfbe18b609d0c
Yu081197 <81637903+Yu081197@users.noreply.github.com>
2022-03-31 14:35:24 +0900

    add: signup layout 구현

--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -3,35 +3,51 @@ import style from './SignUp.module.css';
 
 function SignUp() {
   return (
-    <div className={style.loginbox}>
-      <div className={style.logintitlebox}>
-        <h1 className={style.logintitle}>L O G I N</h1>
-        <h2 className={style.loginsubtitle}>로그인</h2>
+    <div className={style.signupbox}>
+      <div className={style.signuptitlebox}>
+        <h1 className={style.signuptitle}>J O I N</h1>
+        <h2 className={style.signupsubtitle}>회원가입</h2>
       </div>
 
-      <div className={style.logininputbox}>
-        <input
-          className={style.logininput}
-          type="text"
-          name="ID"
-          id="id"
-          placeholder="이메일 아이디"
-        />
-        <input
-          className={style.logininput}
-          type="password"
-          name="Password"
-          id="pw"
-          placeholder="비밀번호"
-        />
+      <div className={style.signupinputbox}>
+        <div className={style.signupsmallinput}>
+          <span>이메일</span>
+          <input
+            className={style.signupinput}
+            type="text"
+            name="ID"
+            id="id"
+            placeholder="이메일 아이디"
+          />
+        </div>
+        <div className={style.signupsmallinput}>
+          <span>이메일</span>
+          <input
+            className={style.signupinput}
+            type="password"
+            name="Password"
+            id="pw"
+            placeholder="비밀번호"
+          />
+        </div>
+        <div className={style.signupsmallinput}>
+          <span>이메일</span>
+          <input
+            className={style.signupinput}
+            type="password"
+            name="Password"
+            id="pw"
+            placeholder="비밀번호"
+          />
+        </div>
       </div>
 
-      <div className={style.loginbuttonbox}>
-        <button className={style.loginbutton} id="login-button">
+      <div className={style.signupbuttonbox}>
+        <button className={style.signupbutton} id="signup-button">
           L O G I N
         </button>
         <button
-          className={style.loginbutton}
+          className={style.signupbutton}
           id="non-member-reservation-button"
         >
           비회원 예약 조회
@@ -39,12 +55,12 @@ function SignUp() {
       </div>
 
       <div className={style.linkbox}>
-        <span className={style.loginfindlink}>회원가입</span>
-        <span className={style.loginfindlink}>비밀번호 찾기</span>
+        <span className={style.signupfindlink}>회원가입</span>
+        <span className={style.signupfindlink}>비밀번호 찾기</span>
       </div>
 
-      <div className={style.snsloginbox}>
-        <span className={style.snsloginlink}>SNS 계정으로 로그인하기</span>
+      <div className={style.snssignupbox}>
+        <span className={style.snssignuplink}>SNS 계정으로 로그인하기</span>
         <div className={style.snsemoticonbox}>
           <span className={style.emoticonbox}>
             a<i />
