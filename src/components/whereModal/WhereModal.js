7a17e610d5bc86dffd38c19554d60d2e53f0e4ae
김수빈 <ksb334ksb@gmail.com>
2022-04-01 17:58:27 +0900

    modify : where modal 디자인 추가

--- src/components/whereModal/WhereModal.js
+++ src/components/whereModal/WhereModal.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import style from './WhereModal.module.css';
+import {
+  AiOutlineSearch,
+  AiOutlineClose,
+  AiOutlineArrowRight,
+} from 'react-icons/ai';
+import BlackButton from '../BlackButton/BlackButton';
 
 function WhereModal({ modalRef, closeModal }) {
   return (
@@ -7,13 +13,24 @@ function WhereModal({ modalRef, closeModal }) {
       <div className={style.modal}>
         <div className={style.titleWrapper}>
           <div className={style.modalTitle}>
-            <p>어디로떠날까요?</p>
-            <button id="modalClose1" onClick={closeModal}>
-              닫기
-            </button>
+            <p className={style.whereText}>어디로떠날까요?</p>
+            <AiOutlineClose
+              className={style.closeModal}
+              size="50"
+              id="modalClose1"
+              onClick={closeModal}
+            />
           </div>
         </div>
-        <input className={style.inputLocation} />
+        <div className={style.inputWrapper}>
+          <AiOutlineSearch size="24" className={style.searchIcon} />
+          <div className={style.inputShape}>
+            <input
+              className={style.inputText}
+              placeholder="원하는 스테이/지역을 검색해 보세요."
+            />
+          </div>
+        </div>
         <div className={style.location}>
           <div className={style.locationWrapper}>
             <p className={style.domestic}>국내</p>
@@ -53,7 +70,12 @@ function WhereModal({ modalRef, closeModal }) {
               <li className={style.city}>울산</li>
             </ul>
           </div>
-          <div className={style.space} />
+        </div>
+        <div className={style.btnWrapper}>
+          <BlackButton
+            className={style.searchBtn}
+            content={`search ${(<AiOutlineArrowRight />)}`}
+          />
         </div>
       </div>
     </div>
