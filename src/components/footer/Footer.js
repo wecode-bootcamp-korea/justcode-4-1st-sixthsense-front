486e3ec1c76cf69431bc7e7cce5c6530f820de2e
Yumalup <sonaki970811@gmail.com>
2022-04-02 22:59:27 +0900

    add: 컨플릭트 해결

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,47 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import style from './Footer.module.css';
-import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 function Footer() {
-  return (
-    <div className={style.footer}>
-      <div className={style.footerLeft}>
-        <div className={style.logo}>
-          S T A Y <br />
-          FOREST
-        </div>
-        <div className={style.footerDetail}>
-          상호 : ㈜스테이포레스트 &nbsp;&nbsp; 대표자 : 홍길동
-          <br />
-          서울특별시 서울구 서울역로1길 12, 1층(서울동) &nbsp;&nbsp; TEL:
-          1234-5678 &nbsp;&nbsp; help@stayforest.com
-          <br />
-          사업자등록번호: 123-45-67890&nbsp;&nbsp;통신판매업신고 :
-          제1234-서울종로-1234호[사업자정보확인]
-          <br />
-          관광사업자등록 : 일반여행업 1234-123456호(종로구청)
-        </div>
-        <div className={style.copyRight}>© STAYFOREST</div>
-      </div>
-      <div className={style.footerRight}>
-        <div className={style.icon}>
-          <FaFacebookF />
-          &nbsp;
-          <FaInstagram />
-          &nbsp;
-          <FaYoutube />
-        </div>
-        <div className={style.list}>
-          <Link to="/https://velog.io/@shorrysorry">김수빈</Link>
-          <Link to="/https://velog.io/@sonaki0811">유다송</Link>
-          <Link to="/https://velog.io/@xcc629">임근홍</Link>
-          <Link to="/https://velog.io/@hani2525">전하은</Link>
-          <Link to="/https://velog.io/@jml22">이정민</Link>
-        </div>
-      </div>
-    </div>
-  );
+  return <div />;
 }
 
 export default Footer;
--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import style from './Detail.module.scss';
-import DetailTop from './detailTop/DetailTop';
-import Rooms from './rooms/Rooms';
-import Info from './info/Info';
-import Special from './special/Special';
-import Places from './places/Places';
-import FAQ from './faq/FAQ';
 
 function Detail() {
-  return (
-    <div className={style.detailWrapper}>
-      <DetailTop />
-      <Rooms />
-      <Info />
-      <Special />
-      <Places />
-      <FAQ />
-    </div>
-  );
+  return <div />;
 }
 
 export default Detail;
