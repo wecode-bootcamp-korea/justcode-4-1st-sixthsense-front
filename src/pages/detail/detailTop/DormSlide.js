9046a15a364c590069e0662fc1680c7a8c412d24
전하은 <hani@Hanis-Mac-Book.local>
2022-04-08 10:29:11 +0900

    Update: 초기값 설정

--- src/pages/detail/detailTop/DormSlide.js
+++ src/pages/detail/detailTop/DormSlide.js
@@ -3,37 +3,20 @@ import style from './DormSlide.module.css';
 import classNames from 'classnames/bind';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
-const DormSlide = ({ dormName }) => {
+const DormSlide = ({ dormitoryName, images, comment, city, district }) => {
   const [current, setCurrent] = useState(0);
-
   const cx = classNames.bind(style);
-
-  const slide = [
-    {
-      id: 1,
-      url: 'https://images.unsplash.com/photo-1598605272254-16f0c0ecdfa5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80',
-    },
-    {
-      id: 2,
-      url: 'https://images.unsplash.com/photo-1623718649591-311775a30c43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    },
-    {
-      id: 3,
-      url: 'https://images.unsplash.com/photo-1584132869994-873f9363a562?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-    },
-  ];
-
   const next = () => {
-    setCurrent(() => (current === slide.length - 1 ? 0 : current + 1));
+    setCurrent(() => (current === images.length - 1 ? 0 : current + 1));
   };
 
   const prev = () => {
-    setCurrent(() => (current === 0 ? slide.length - 1 : current - 1));
+    setCurrent(() => (current === 0 ? images.length - 1 : current - 1));
   };
 
   return (
     <div className={cx('container')}>
-      {slide.map((el, idx) => {
+      {images.map((el, idx) => {
         return (
           <div
             className={cx(
@@ -43,27 +26,33 @@ const DormSlide = ({ dormName }) => {
             key={el.id}
           >
             {idx === current && (
-              <img className={cx('slideImg')} src={el.url} alt={el.id} />
+              <img className={cx('slideImg')} src={el} alt="dormitory images" />
             )}
           </div>
         );
       })}
       <div className={cx('prevBtn', 'btnWrapper')} onClick={prev}>
-        <IoIosArrowBack className={cx('btn')} size="20" color="white" />
+        <IoIosArrowBack className={cx('btn', 'back')} size="20" color="white" />
       </div>
       <div className={cx('nextBtn', 'btnWrapper')} onClick={next}>
-        <IoIosArrowForward className={cx('btn')} size="20" color="white" />
+        <IoIosArrowForward
+          className={cx('btn', 'forward')}
+          size="20"
+          color="white"
+        />
       </div>
       <div className={cx('description')}>
-        <p className={cx('quote')}>여름 햇살 아래 구옥이 품은 빈티지</p>
+        <p className={cx('quote')}>{comment}</p>
         <div className={cx('category')}>
           <p className={cx('categoryName', 'magazine')}>MAGAZINE</p>
           <p className={cx('categoryName', 'pick')}>PICK</p>
           <p className={cx('categoryName', 'promotion')}>PROMOTION</p>
         </div>
         <div className={cx('dormInfo')}>
-          <p className={cx('dormName')}>'dormName'</p>
-          <p className={cx('location')}>'location'/ 'district'</p>
+          <p className={cx('dormName')}>{dormitoryName}</p>
+          <p className={cx('location')}>
+            {city}/ {district}
+          </p>
         </div>
       </div>
     </div>
